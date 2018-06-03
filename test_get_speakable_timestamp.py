9a3b92173b095d4bb305dfdc9036affa52c699c4
Jacob Luszcz <jacob.luszcz@gmail.com>
2018-06-03 07:22:05 -0400

    Add tests for get_speakable_timestamp

---
+++ test_get_speakable_timestamp.py
@@ -0,0 +1,27 @@
+import jakesky
+
+from datetime import datetime
+
+
+def _get_timestamp(hour):
+    return datetime(2018, 6, 3, hour)
+
+
+def test_noon():
+    ts = _get_timestamp(12)
+    assert 'noon' == jakesky.get_speakable_timestamp(ts)
+
+
+def test_midnight():
+    ts = _get_timestamp(0)
+    assert 'midnight' == jakesky.get_speakable_timestamp(ts)
+
+
+def test_morning():
+    ts = _get_timestamp(9)
+    assert '9 AM' == jakesky.get_speakable_timestamp(ts)
+
+
+def test_evening():
+    ts = _get_timestamp(21)
+    assert '9 PM' == jakesky.get_speakable_timestamp(ts)
