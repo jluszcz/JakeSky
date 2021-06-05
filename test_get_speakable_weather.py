5149ea2e5f49e45842e79a6ee25f888c70a95899
Jacob Luszcz <jacob.luszcz@gmail.com>
2021-06-05 07:48:54 -0400

    Add test for speakable weather

--- test_get_speakable_weather.py
+++ test_get_speakable_weather.py
@@ -3,3 +3,7 @@ import jakesky
 def test_get_speakable_weather_summary():
     assert 'Drizzling' == jakesky.get_speakable_weather_summary('Drizzle')
     assert 'Raining' == jakesky.get_speakable_weather_summary('Raining')
+
+
+def test_get_speakable_weather():
+    assert '65 and Sunny' == jakesky.get_speakable_weather(jakesky.Weather('2021-06-05T00:00:00Z', 'Sunny', 65.45))
